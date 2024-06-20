3c8240d9ca1ec00c9dc2af143e9e502a199e15ef
giuk-kim <gimgiug87@gamil.com>
2024-06-20 11:01:49 +0900

    encryption commit

--- src/main/java/com/exam/controller/MemberController.java
+++ src/main/java/com/exam/controller/MemberController.java
@@ -4,19 +4,18 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.exam.dto.MemberDTO;
 import com.exam.service.MemberService;
 
 
 @Controller
-@SessionAttributes(names = {"login"})
 public class MemberController {
 	
 	Logger logger = LoggerFactory.getLogger(getClass());
@@ -30,8 +29,7 @@ public class MemberController {
 	@GetMapping("/signup")
 	public String signupForm(ModelMap m) {
 		
-		MemberDTO dto = new MemberDTO();
-		m.addAttribute("memberDTO", dto);
+		m.put("memberDTO", new MemberDTO());
 		
 		return "memberForm";
 	}
@@ -43,6 +41,10 @@ public class MemberController {
 		}
 		//DB연동
 		logger.info("logger:signup:{}", dto);
+		
+		// 비번 암호화 필수
+		String encptPw = new BCryptPasswordEncoder().encode(dto.getPasswd());
+		dto.setPasswd(encptPw);
 		
 		int n = memberService.memberAdd(dto);
 		
--- src/main/resources/META-INF/resources/WEB-INF/views/member/memberForm.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/views/member/memberForm.jsp
@@ -40,6 +40,7 @@
 				<input type="password" class="form-control" name="passwd2"
 					id="passwd2">
 			</div>
+		</div>	
 			<div class="col-sm-3">
 				<span id="pwdcheck" class="fs-5"></span>
 			</div>
