732ca754cb0ebb2e4d274cacc5e3610c9e706112
giuk-kim <gimgiug87@gamil.com>
2024-06-20 13:50:54 +0900

    myPage commit

--- src/main/java/com/exam/controller/MemberController.java
+++ src/main/java/com/exam/controller/MemberController.java
@@ -4,13 +4,14 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.exam.dto.MemberDTO;
 import com.exam.service.MemberService;
 
@@ -45,19 +46,42 @@ public class MemberController {
 		// 비번 암호화 필수
 		String encptPw = new BCryptPasswordEncoder().encode(dto.getPasswd());
 		dto.setPasswd(encptPw);
-		
 		int n = memberService.memberAdd(dto);
 		
 		return "redirect:main";
 	}
 	
-	@GetMapping("/mypage")
-	public String mypage(ModelMap m) {
+//	@GetMapping("/mypage")
+//	public String mypage(ModelMap m) {
+//		
+//		// 세션에 저장된 MemberDTO 얻기
+//		MemberDTO dto = (MemberDTO)m.getAttribute("login");
+//		logger.info("loggerLmypage:{}", dto);
+//		String userid = dto.getUserid();
+//		
+//		MemberDTO searchDTO = memberService.mypage(userid);
+//		m.addAttribute("login", searchDTO);
+//		
+//		return "mypage";
+//	}
+	
+	@GetMapping(value={"/mypage"})
+	public String myPage(ModelMap m) {
 		
-		// 세션에 저장된 MemberDTO 얻기
-		MemberDTO dto = (MemberDTO)m.getAttribute("login");
-		logger.info("loggerLmypage:{}", dto);
-		String userid = dto.getUserid();
+		//Authentication의 실제 데이터는
+		//
+		// UsernamePasswordAuthenticationToken token = null;
+		// token = new UsernamePasswordAuthenticationToken(MemberDTO, null, list); 
+		// token = new UsernamePasswordAuthenticationToken(Principal, null, list); 
+		
+		
+		// AuthProvider에 저장된 Authentication 얻자
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		logger.info("logger:Authentication:{}", auth);
+		MemberDTO memberDTO =(MemberDTO)auth.getPrincipal();
+		logger.info("logger:memberDTO:{}", memberDTO);
+		
+		String userid = memberDTO.getUserid();
 		
 		MemberDTO searchDTO = memberService.mypage(userid);
 		m.addAttribute("login", searchDTO);
